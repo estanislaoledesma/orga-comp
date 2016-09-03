5b2256b867856ed92c54ccab5fade14d226370ea
Estanislao <estanislaomledesma@gmail.com>
2016-09-03 20:59:09 -0300

    Agregados primeros manejos de parámetros.

--- tp0.c
+++ tp0.c
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <stdio.h>
+
 #define N 10
 
 
@@ -6,9 +9,44 @@ typedef struct complejo{
 	float parteImaginaria;
 }complejo;
 
-int main(int argc, char* argv[]){
+int main(int argc, const char* argv[]){
 	/* Validar parámetros y crear archivo PGM de salida */
-	char* archivoSalida;
+	char* archivoSalida = NULL;
+	char* resolucion = "640x480";
+	char* anchoRectangulo = "4";
+	char* altoRectangulo = "4";
+	char* centro = "0+0i";
+	char* c = "0,285−0,01i";
+	if (argc < 3){
+		return 1;
+	}
+	for (int i = 1; i < argc; i++){
+		if((i + 1) < argc){
+			return 1;
+		}
+		if (strcmp(argv [i], "-r") == 0){
+			resolucion = argv [i + 1];
+		}
+		if (strcmp(argv [i], "-c") == 0){
+			centro = argv [i + 1];
+		}
+		if (strcmp(argv [i], "-C") == 0){
+			c = argv [i + 1];
+		}
+		if (strcmp(argv [i], "-w") == 0){
+			anchoRectangulo = argv [i + 1];
+		}
+		if (strcmp(argv [i], "-H") == 0){
+			altoRectangulo = argv [i + 1];
+		}
+		if (strcmp(argv [i], "-r") == 0){
+			archivoSalida = argv [i + 1];
+		}
+		else{
+			return 1;
+		}
+
+	}
 	int anchoResolucion, altoResolucion, anchoRectangulo, altoRectangulo;
 	complejo* centro, c;
 	complejo* origen = malloc(sizeof(complejo));
