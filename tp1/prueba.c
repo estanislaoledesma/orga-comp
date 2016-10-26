2b827dd62ac0aebab7e7914c628fca54c4d43981
Agustin <agus.luques@hotmail.com>
2016-10-26 16:03:34 -0300

    subo el tp inicializado, tambien subo un excel para tener el stackframe a mano y un codigo de prueba que use para ver como se pasan estructuras

---
+++ tp1/prueba.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+typedef struct hola
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	int h;
+	int i;
+	int j;
+	int k;
+	int l;
+}hola_t;
+
+void hola_mundo(hola_t* params){
+	params->a = 3;
+	params->e = params->f + params->g;
+	printf("Hola\n");
+}
+
+int main(int argc, char const *argv[])
+{
+	hola_t params;
+	params.a = 1;
+	params.b = 1;
+	params.c = 1;
+	params.d = 1;
+	params.e = 1;
+	params.f = 1;
+	params.g = 1;
+	
+	
+	hola_mundo(&params);
+	return 0;
+}
